1f69c704b3f56ea55a8ef24cb4d6430be14b201c
sergeilebedev <selebedev@alfabank.ru>
2023-03-17 13:53:36 +0300

    First commit

---
+++ src/main/java/com/taskforwaruniverse/TaskForWarUniverseApplication.java
@@ -0,0 +1,13 @@
+package com.taskforwaruniverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskForWarUniverseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskForWarUniverseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taskforwaruniverse/TaskForWarUniverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskforwaruniverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskForWarUniverseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
